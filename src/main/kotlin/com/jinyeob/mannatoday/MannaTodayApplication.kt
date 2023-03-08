9f25c770c9f4e2d5e52fb400a8515b5c9a3de414
nathankim0 <jinyeob07@gmail.com>
2023-03-08 18:24:09 +0900

    init

---
+++ src/main/kotlin/com/jinyeob/mannatoday/MannaTodayApplication.kt
@@ -0,0 +1,11 @@
+package com.jinyeob.mannatoday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MannaTodayApplication
+
+fun main(args: Array<String>) {
+    runApplication<MannaTodayApplication>(*args)
+}
---
+++ src/test/kotlin/com/jinyeob/mannatoday/MannaTodayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jinyeob.mannatoday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MannaTodayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
